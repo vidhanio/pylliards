af871a0e5a02ca5c9c36ba91e3b33237e14909ff
Vidhan Bhatt <me@vidhan.io>
2022-10-08 15:17:02 -0400

    feat: add boilerplate

---
+++ pylliards/engine/__init__.py
@@ -0,0 +1,10 @@
+class Ball:
+    def __init__(self, x: int, y: int):
+        self.x: int
+        self.y: int
+
+        self.vx: int
+        self.vy: int
+
+        self.x = x
+        self.y = y
