8af0328eb151bef5f59bf505bab0563faf0629f2
Vidhan Bhatt <me@vidhan.io>
2022-10-08 15:30:39 -0400

    docs: add comments

--- pylliards/engine/__init__.py
+++ pylliards/engine/__init__.py
@@ -1,5 +1,16 @@
 class Ball:
+    """
+    A ball.
+    """
+
     def __init__(self, x: int, y: int):
+        """
+        Create a ball using x and y coordinates.
+
+        Args:
+            x: The x coordinate of the ball
+            y: The y coordinate of the ball
+        """
         self.x: int
         self.y: int
 
